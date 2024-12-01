74ca1521984f31b4cfe72d6da2c7e054d874b741
Misbachul Huda <misbachulhuda67@gmail.com>
2024-12-01 21:57:08 +0700

    Update inferenceService.js

--- backend-api/src/services/inferenceService.js
+++ backend-api/src/services/inferenceService.js
@@ -1,1 +1,28 @@
+const tf = require("@tensorflow/tfjs-node");
+const InputError = require("../exceptions/InputError");
+
+async function predictClassification(model, image) {
+    try {
+        const tensor = tf.node
+            .decodeJpeg(image)
+            .resizeNearestNeighbor([224, 224])
+            .expandDims()
+            .toFloat();
+
+        const prediction = model.predict(tensor);
+        const score = await prediction.data();
+        const confidenceScore = Math.max(...score) * 100;
+
+        const label = confidenceScore >= 0.5 ? "Organic" : "Non-Organic";
+
+        const suggestion =
+            label === "Organic" ? "Buanglah sampah organik" : "Buanglah sampah non organik";
+
+        return { label, suggestion };
+    } catch (error) {
+        throw new InputError(`Terjadi kesalahan input: ${error.message}`);
+    }
+}
+
+module.exports = predictClassification;
 
