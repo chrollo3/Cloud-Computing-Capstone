9dc8a5408c8fe8fbcfe0e0e4619bda61fd6c7eb4
Misbachul Huda <misbachulhuda67@gmail.com>
2024-12-01 21:56:27 +0700

    Update routes.js

--- backend-api/src/server/routes.js
+++ backend-api/src/server/routes.js
@@ -1,1 +1,28 @@
+const {
+    postPredictHandler,
+    getPredictHistoriesHandler,
+} = require("../server/handler");
+
+const routes = [
+    {
+        path: "/predict",
+        method: "POST",
+        handler: postPredictHandler,
+        options: {
+            payload: {
+                /*Mengizinkan data berupa gambar*/
+                allow: "multipart/form-data",
+                maxBytes: 1000000,
+                multipart: true,
+            },
+        },
+    },
+    {
+        path: "/predict/histories",
+        method: "GET",
+        handler: getPredictHistoriesHandler,
+    },
+];
+
+module.exports = routes;
 
