5fb83966c6658a9e7483a7d32912100b38a43bfa
Misbachul Huda <misbachulhuda67@gmail.com>
2024-12-01 21:57:35 +0700

    Update storeData.js

--- backend-api/src/services/storeData.js
+++ backend-api/src/services/storeData.js
@@ -1,1 +1,11 @@
+const { Firestore } = require("@google-cloud/firestore");
+
+async function storeData(id, data) {
+    const db = new Firestore({ databaseId: 'testing-cc' });
+
+    const predictCollection = db.collection("predictions");
+    return predictCollection.doc(id).set(data);
+}
+
+module.exports = storeData;
 
